6ae4aeb5d612c77b8d74b173e15126998785a4f6
Sreevalli Viswanatha <sviswanatha@pennfoster.edu>
2025-08-10 13:02:24 -0500

    feat: Activate Summer Beach theme and background animations

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ import { AuthProvider } from "./contexts/AuthContext.jsx";
 import ProtectedRoute from "./components/global-components/ProtectedRoute/ProtectedRoute.jsx";
 import Homepage from "./components/global-components/Homepage/Homepage.jsx";
 import ExistingRuleCategories from "./pages/ExistingRuleCategories/ExistingRuleCategories.jsx";
-import Bubbles from "./themes/OceanTheme/Bubbles.jsx";
+import BeachAnimations from "./themes/SummerBeachTheme/BeachAnimations.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Bubbles />
+        <BeachAnimations />
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Navigate replace to="/home" />} />
