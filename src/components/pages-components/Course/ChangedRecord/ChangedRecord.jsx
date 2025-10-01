6c9deaf249016758cc26b7f94e353a509d03eecd
Shandilya Nookala <shandilya.nookala@gmail.com>
2025-10-01 15:43:48 -0500

    Fixed the editing bug
    When we add a new record, then decide to edit, the edited record doesn't show in ChangedRecord. This fixes that

--- src/components/pages-components/Course/ChangedRecord/ChangedRecord.jsx
+++ src/components/pages-components/Course/ChangedRecord/ChangedRecord.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import WorkRules from "../WorkRules/WorkRules";
 import StandardWork from "../StandardWork/StandardWork";
 import { Button } from "@mui/material";
@@ -16,6 +16,13 @@ export default function ChangedRecord({ taskId }) {
   );
   const [work, setWork] = useState(changedRecordData?.work);
   const scrollContainerRef = useRef(null);
+
+  useEffect(
+    function () {
+      setWork(changedRecordData?.work);
+    },
+    [changedRecordData?.work]
+  );
 
   async function handleSaveRecord() {
     const newWork = Array.isArray(work)
